e1645cc6c19efcbddb99395f9acb3324b409f36e
javier.sanchez <kornjaviersanchezb@gmail.com>
2025-01-27 00:37:41 -0500

    create db

---
+++ lib/db.ts
@@ -0,0 +1,18 @@
+import { Pool } from 'pg';
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export async function query(text: string, params?: (string | number | boolean | null)[]) {
+  const client = await pool.connect();
+  try {
+    const res = await client.query(text, params);
+    return res;
+  } finally {
+    client.release();
+  }
+}
