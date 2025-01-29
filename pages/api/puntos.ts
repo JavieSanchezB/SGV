1ad1d525a0e8a9cc45fa3e046b804ff5c8f840f7
javier.sanchez <ing.javier.sanchez.b@gmail.com>
2025-01-29 00:43:32 -0500

    fix para id

--- pages/api/puntos.ts
+++ pages/api/puntos.ts
@@ -28,33 +28,42 @@ export default async function handler(
   }
 
   try {
-    let rows: { id_omt: number; nombre_del_establecimiento: string; direccion: string; telefono: string }[] = [];
+    let rows: {
+      id_omt: number;
+      nombre_del_establecimiento: string;
+      nombre_del_propietario: string;
+      cc_del_propietario: string;
+      nit_del_propietario: string;
+      tel_del_propietario: string;
+      direccion: string;
+      barrio: string;
+      nombre_del_administrador: string;
+      tel_del_administrador: string;
+      nombre_del_encargado: string;
+      tel_del_encargado: string;
+      fechas_de_pago: string;
+      latitud: number;
+      longitud: number;
+    }[] = [];
     if (idOmt) {
       // Realiza la consulta a la tabla "establecimientos" por id_omt
       console.log('Consultando por id_omt:', idOmt);
       const result = await query(
-        'SELECT * FROM establecimientos WHERE id_omt = $1',
+        'SELECT id_omt, nombre_del_establecimiento, nombre_del_propietario, cc_del_propietario, nit_del_propietario, tel_del_propietario, direccion, barrio, nombre_del_administrador, tel_del_administrador, nombre_del_encargado, tel_del_encargado, fechas_de_pago, latitud, longitud FROM establecimientos WHERE id_omt = $1',
         [idOmt]
       );
       rows = result.rows;
       console.log('Resultado de la consulta por id_omt:', rows);
     } else if (nombreEstablecimiento) {
       // Realiza la consulta a la tabla "establecimientos" por nombre_del_establecimiento
+      console.log('Consultando por nombre_del_establecimiento:', nombreEstablecimiento);
       const result = await query(
-        'SELECT * FROM establecimientos WHERE nombre_del_establecimiento ILIKE $1',
+        'SELECT id_omt, nombre_del_establecimiento, nombre_del_propietario, cc_del_propietario, nit_del_propietario, tel_del_propietario, direccion, barrio, nombre_del_administrador, tel_del_administrador, nombre_del_encargado, tel_del_encargado, fechas_de_pago, latitud, longitud FROM establecimientos WHERE nombre_del_establecimiento ILIKE $1',
         [`%${nombreEstablecimiento}%`]
       );
       rows = result.rows;
     }
 
-    if (rows.length === 0) {
-      return res.status(404).json({
-        success: false,
-        error: 'No se encontró información para los parámetros proporcionados.',
-      });
-    }
-
-    // Verifica si se encontraron resultados
     if (rows.length === 0) {
       return res.status(404).json({
         success: false,
